b52a5708c0efeb4ad33bcbb4ae4874322e39c3ea
Nvnrocky <narasimhanagulapati01@gmail.com>
2023-11-20 22:30:00 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def srujuna(request):
+    return render(request,'srujuna.html')
+
+def prashanth(request):
+    return render(request,'prashanth.html')
